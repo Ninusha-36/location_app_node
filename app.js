b264a2c4672339d7e9b5e687aa6f7779aca39a53
Ninusha-36 <neenusha369@gmail.com>
2026-05-28 17:51:13 +0530

    basic file

---
+++ app.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const dns = require('dns')
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use((error, req, res, next) => {
+    if(req.file){
+      fs.unlink(req.file.path, err => {
+        console.log(err)
+      })
+    }
+    if(res.headerSent){
+        return next(error)
+    }
+    res.status(error.status || 505)
+    res.json({message : error.message || "An unknown error occured!"})
+})
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+
+  mongoose
+  .connect(
+    `mongodb+srv://ninushaneu_db_user:passdfdsfdsf@cluster0.oewi9uh.mongodb.net/mern?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+    console.log('Connected to database!');
+  })
+  .catch(err => {
+    console.log('123'+err);
+  });
---
+++ routes/places-routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { check } = require('express-validator');
+
+const placesControllers = require('../controllers/places-controllers');
+
+const CheckAuth = require('../middleware/check-auth')
+const fileUpload = require('../middleware/file-upload');
+
+
+const router = express.Router();
+
+router.get('/', placesControllers.getAllPlace);
+
+router.get('/:pid', placesControllers.getPlaceById);
+
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+router.use(CheckAuth)
+
+router.post(
+  '/',
+  fileUpload.single('image'),
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
+
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
+
+router.delete('/:pid', placesControllers.deletePlace);
+
+module.exports = router;
